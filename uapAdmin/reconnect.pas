6228106f0977ef9710a4c3e993e539f87a24a3df
SergeKretov <60303226+SergeKretov@users.noreply.github.com>
2020-02-03 10:07:49 +0700

    Create reconnect.pas

---
+++ uapAdmin/reconnect.pas
@@ -0,0 +1,25 @@
+unit reconnect;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  Treconnectform = class(TForm)
+    StaticText1: TStaticText;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  reconnectform: Treconnectform;
+
+implementation
+
+{$R *.dfm}
+
+end.
