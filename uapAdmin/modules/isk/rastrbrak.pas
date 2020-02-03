c76c25754a840ded8fe1bb6fda0461183846abd6
SergeKretov <60303226+SergeKretov@users.noreply.github.com>
2020-02-03 09:43:59 +0700

    Create rastrbrak.pas

---
+++ uapAdmin/modules/isk/rastrbrak.pas
@@ -0,0 +1,31 @@
+unit rastrbrak;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, QuickRpt, QRCtrls;
+
+type
+  TForm2 = class(TForm)
+    QuickRep1: TQuickRep;
+    ColumnHeaderBand1: TQRBand;
+    sud: TQRLabel;
+    istec: TQRLabel;
+    otvetchik: TQRLabel;
+    QRLabel1: TQRLabel;
+    QRLabel2: TQRLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
