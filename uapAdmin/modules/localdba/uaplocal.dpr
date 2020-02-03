9f4df259c79790273acfb81d792ae1f3413b2589
SergeKretov <60303226+SergeKretov@users.noreply.github.com>
2020-02-03 10:03:50 +0700

    Create uaplocal.dpr

---
+++ uapAdmin/modules/localdba/uaplocal.dpr
@@ -0,0 +1,14 @@
+program uaplocal;
+
+uses
+  Forms,
+  uaplocalmain in 'uaplocalmain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Менеджер архивов БД';
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
